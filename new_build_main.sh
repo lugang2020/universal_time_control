34ca6244efbfcf882a92aaee1a894e5f8312cbe8
abdurrahimbrsh <abdurrahimbrsh01@gmail.com>
2020-08-21 12:25:35 +0200

    first build for debugging

--- new_build_main.sh
+++ new_build_main.sh
@@ -3,9 +3,9 @@ set -e
 
 # Future dev: Apologies for the hardcoded path :)
 # I have a strange Node dev environment and need to juggle various versions.
-C:/Users/JonathanWork/AppData/Roaming/nvm/v13.7.0/node.exe ./build_cimgui.mjs
-C:/Users/JonathanWork/AppData/Roaming/nvm/v13.7.0/node.exe ./build_speedhack_win_node.mjs
-C:/Users/JonathanWork/AppData/Roaming/nvm/v13.7.0/node.exe ./build_main_win_node.mjs
+/C/nodejs/node.exe ./build_cimgui.mjs
+/C/nodejs/node.exe ./build_speedhack_win_node.mjs
+/C/nodejs/node.exe ./build_main_win_node.mjs
 
 # gdb -ex run test_win.exe
 ./test_win.exe
